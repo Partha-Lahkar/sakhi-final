16a9cfb2dc1a65b055d0db4619873cab7faf2dcd
Partha-Lahkar <parthalahkar35@gmail.com>
2024-07-10 22:38:25 +0530

    pushing

--- lib/screens/button_screen.dart
+++ lib/screens/button_screen.dart
@@ -1,107 +1,244 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:async';
+import 'dart:io';
 
-class ButtonScreen extends StatelessWidget {
+class ButtonScreen extends ConsumerStatefulWidget {
   const ButtonScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return _ButtonScreenState();
+  }
+}
+
+class _ButtonScreenState extends ConsumerState<ButtonScreen> {
+  bool _isAlarmEnabled = false;
+  Timer? _timer;
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeNotifications();
+  }
+
+  void _initializeNotifications() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings(
+            '@mipmap/ic_launcher'); // Ensure this icon exists
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+    );
+
+    await _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+    await _requestNotificationPermission();
+  }
+
+  Future<void> _requestNotificationPermission() async {
+    if (Platform.isAndroid && Platform.version.contains('33')) {
+      final AndroidFlutterLocalNotificationsPlugin? androidImplementation =
+          _flutterLocalNotificationsPlugin
+              .resolvePlatformSpecificImplementation<
+                  AndroidFlutterLocalNotificationsPlugin>();
+      final bool? granted = await androidImplementation?.requestPermission();
+      if (granted != null && !granted) {
+        print("Notification permission denied");
+      }
+    }
+  }
+
+  void _scheduleAlarm() {
+    print("Scheduling alarm"); // Debug print
+    _timer?.cancel(); // Cancel any existing timer
+    if (_isAlarmEnabled) {
+      _timer = Timer.periodic(const Duration(minutes: 2), (timer) {
+        print("Timer triggered"); // Debug print
+        _showNotification();
+      });
+    }
+  }
+
+  Future<void> _showNotification() async {
+    print("Attempting to show notification"); // Debug print
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'alarm_channel',
+      'Alarm Channel',
+      channelDescription: 'This channel is used for alarm notifications',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await _flutterLocalNotificationsPlugin.show(
+      0,
+      'Alarm',
+      'Dont Forget to Check your Bestie ~ Team Sakhi',
+      platformChannelSpecifics,
+    );
+    print("Notification shown"); // Debug print
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 227, 204, 216),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/places');
-              },
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    width: 120, // Adjust size as needed
-                    height: 160, // Adjust size as needed
-                    decoration: BoxDecoration(
-                      color: Colors.white,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(
+                top: 100.0), // Adjust the top padding as needed
+            child: Wrap(
+              alignment: WrapAlignment.center,
+              spacing: 20.0, // Adjust spacing between buttons
+              runSpacing: 50.0, // Adjust spacing between rows
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/places');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10.0),
-                      image: const DecorationImage(
-                        image: AssetImage(
-                            'lib/assets/findway.png'), // Replace with your image path
-                        fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        width: 120, // Adjust size as needed
+                        height: 160, // Adjust size as needed
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                          image: const DecorationImage(
+                            image: AssetImage(
+                                'lib/assets/findway.png'), // Replace with your image path
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                          height: 8), // Adjust spacing between image and text
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        width: 120, // Adjust size as needed
+                        height: 160, // Adjust size as needed
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          image: const DecorationImage(
+                            image: AssetImage(
+                                'lib/assets/medicine.png'), // Replace with your image path
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                          height: 8), // Adjust spacing between image and text
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        width: 120, // Adjust size as needed
+                        height: 160, // Adjust size as needed
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          image: const DecorationImage(
+                            image: AssetImage(
+                                'lib/assets/appointment.png'), // Replace with your image path
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                          height: 8), // Adjust spacing between image and text
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // No specific action required when the button is pressed
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    primary: Colors.black, // Background color of the button
+                  ),
+                  child: Container(
+                    width: 120, // Adjust size as needed
+                    height: 168, // Adjust size as needed
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Enable Alarm',
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white), // Text color
+                          ),
+                          Switch(
+                            value: _isAlarmEnabled,
+                            onChanged: (value) {
+                              setState(() {
+                                _isAlarmEnabled = value;
+                              });
+                              _scheduleAlarm();
+                            },
+                            activeColor:
+                                Colors.blue, // Color of the active switch
+                            inactiveTrackColor: Colors
+                                .grey, // Color of the inactive switch track
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                  const SizedBox(
-                      height: 8), // Adjust spacing between image and text
-                ],
-              ),
-            ),
-            const SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('');
-              },
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
                 ),
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    width: 120, // Adjust size as needed
-                    height: 160, // Adjust size as needed
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      image: const DecorationImage(
-                        image: AssetImage(
-                            'lib/assets/medicine.png'), // Replace with your image path
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                      height: 8), // Adjust spacing between image and text
-                ],
-              ),
+              ],
             ),
-            const SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('');
-              },
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    width: 120, // Adjust size as needed
-                    height: 160, // Adjust size as needed
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      image: const DecorationImage(
-                        image: AssetImage(
-                            'lib/assets/appointment.png'), // Replace with your image path
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                      height: 8), // Adjust spacing between image and text
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -3,12 +3,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sakhi/providers/user_places.dart';
 import 'package:sakhi/screens/add_place.dart';
 import 'package:sakhi/widgets/places_list.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'dart:async';
-import 'dart:io';
 
 class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
+
   @override
   ConsumerState<ConsumerStatefulWidget> createState() {
     return _PlacesScreenState();
@@ -17,80 +15,11 @@ class PlacesScreen extends ConsumerStatefulWidget {
 
 class _PlacesScreenState extends ConsumerState<PlacesScreen> {
   late Future<void> _placesFuture;
-  bool _isAlarmEnabled = false;
-  Timer? _timer;
-  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
+
   @override
   void initState() {
     super.initState();
     _placesFuture = ref.read(userPlacesProvider.notifier).loadplaces();
-    _initializeNotifications();
-  }
-
-  void _initializeNotifications() async {
-    const AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings(
-            '@mipmap/ic_launcher'); // Ensure this icon exists
-
-    final InitializationSettings initializationSettings =
-        InitializationSettings(
-      android: initializationSettingsAndroid,
-    );
-
-    await _flutterLocalNotificationsPlugin.initialize(initializationSettings);
-    await _requestNotificationPermission();
-  }
-
-  Future<void> _requestNotificationPermission() async {
-    if (Platform.isAndroid && Platform.version.contains('33')) {
-      final AndroidFlutterLocalNotificationsPlugin? androidImplementation =
-          _flutterLocalNotificationsPlugin
-              .resolvePlatformSpecificImplementation<
-                  AndroidFlutterLocalNotificationsPlugin>();
-      final bool? granted = await androidImplementation?.requestPermission();
-      if (granted != null && !granted) {
-        print("Notification permission denied");
-      }
-    }
-  }
-
-  void _scheduleAlarm() {
-    print("Scheduling alarm"); // Debug print
-    _timer?.cancel(); // Cancel any existing timer
-    if (_isAlarmEnabled) {
-      _timer = Timer.periodic(const Duration(seconds: 10), (timer) {
-        print("Timer triggered"); // Debug print
-        _showNotification();
-      });
-    }
-  }
-
-  Future<void> _showNotification() async {
-    print("Attempting to show notification"); // Debug print
-    const AndroidNotificationDetails androidPlatformChannelSpecifics =
-        AndroidNotificationDetails(
-      'alarm_channel',
-      'Alarm Channel',
-      channelDescription: 'This channel is used for alarm notifications',
-      importance: Importance.max,
-      priority: Priority.high,
-    );
-    const NotificationDetails platformChannelSpecifics =
-        NotificationDetails(android: androidPlatformChannelSpecifics);
-    await _flutterLocalNotificationsPlugin.show(
-      0,
-      'Alarm',
-      'Dont Forget to Check your Bestie ~ Team Sakhi',
-      platformChannelSpecifics,
-    );
-    print("Notification shown"); // Debug print
-  }
-
-  @override
-  void dispose() {
-    _timer?.cancel();
-    super.dispose();
   }
 
   @override
@@ -98,39 +27,31 @@ class _PlacesScreenState extends ConsumerState<PlacesScreen> {
     final userplaces = ref.watch(userPlacesProvider);
 
     return Scaffold(
-        appBar: AppBar(
-          // add place button help
-          title: const Text('Your Places'),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.add),
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (ctx) => const AddPlacescreen()));
-              },
-            ),
-            Switch(
-              value: _isAlarmEnabled,
-              onChanged: (value) {
-                setState(() {
-                  _isAlarmEnabled = value;
-                });
-                _scheduleAlarm();
-              },
-            ),
-          ],
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (ctx) => const AddPlacescreen()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(
+          future: _placesFuture,
+          builder: (context, snapshot) =>
+              snapshot.connectionState == ConnectionState.waiting
+                  ? const Center(child: CircularProgressIndicator())
+                  : PlacesList(
+                      places: userplaces,
+                    ),
         ),
-        body: Padding(
-          // for watching the places in the ui
-          padding: const EdgeInsets.all(8.0),
-          child: FutureBuilder(
-              future: _placesFuture,
-              builder: (context, snapshot) =>
-                  snapshot.connectionState == ConnectionState.waiting
-                      ? const Center(child: CircularProgressIndicator())
-                      : PlacesList(
-                          places: userplaces,
-                        )),
-        ));
+      ),
+    );
   }
 }
